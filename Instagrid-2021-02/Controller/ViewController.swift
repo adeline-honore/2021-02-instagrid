330accbe19eb4cf44835f29fb57281877dc31935
adeline-honore <honore_adeline@yahoo.fr>
2021-03-08 13:49:53 +0100

    portrait et paysage ok

---
+++ Instagrid-2021-02/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagrid-2021-02
+//
+//  Created by adeline pc on 17/02/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
